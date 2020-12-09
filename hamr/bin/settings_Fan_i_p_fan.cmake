85fd27055777186e158a6300c3f9d162152e9b5b
Jason Belt <belt@ksu.edu>
2020-12-09 12:55:40 -0600

    starting point

--- hamr/bin/settings_Fan_i_p_fan.cmake
+++
@@ -1,6 +0,0 @@
-add_definitions(-DCAMKES)
-
-if(TARGET muslc)
-  target_link_libraries(Fan_i_p_fan
-                        muslc)
-endif()
--- hamr/bin/settings_TempSensor_i_p_tempSensor.cmake
+++
@@ -1,6 +0,0 @@
-add_definitions(-DCAMKES)
-
-if(TARGET muslc)
-  target_link_libraries(TempSensor_i_p_tempSensor
-                        muslc)
-endif()
